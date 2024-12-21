058aaaa23a9ec90ad506989f51d361bd6e62eaad
Katchie <41813266+Ka-Da-Sci@users.noreply.github.com>
2024-12-21 23:55:27 +0100

    Add mobile toggle.

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,50 +1,90 @@
-import { FC } from 'react';
-import { NavLink } from 'react-router-dom';
-import logo from '../../assets/images/logo-trans.svg';
-import menuClose from '../../assets/images/icon-close-menu.svg';
-import MenuOpen from '../../assets/images/icon-menu.png';
-import classes from './Header.module.css';
-import Logo from './Logo';
-import ImageWrapper from '../UI/ImageWrapper';
+import { FC, useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo-trans.svg";
+import classes from "./Header.module.css";
+import Logo from "./Logo";
+import MobileToggle from "./MobileToggle";
+
+// interface HeaderProps {
+//     toggle?: boolean;
+// }
 
 const Header: FC = () => {
+  const [toggleMobile, setToggleMobile] = useState(false);
+  //   const [resizeToggleMobile, setResizeToggleMobile] = useState(false);
 
-    return (
-        <div id='header' className={classes.header}>
-            <NavLink to='/'>
-                <Logo imageUrl={logo}/>
-            </NavLink>
-            <nav className={classes.nav}>
+  useEffect(() => {
+    const updateToggleMobile = () => {
+      const width = window.innerWidth;
+      const navBar = document.getElementById("header");
+      console.log(navBar);
 
-                <NavLink className={({isActive}) => (isActive ? classes.active: '')} to='/'>
-                    <h1>Home</h1>
-                </NavLink>
+      if (width <= 640) {
+        setToggleMobile(false);
+      } else {
+        setToggleMobile(true);
+      }
+    };
 
-                <NavLink className={({isActive}) => (isActive ? classes.active : '')} to='/portfolios'>
-                    <h1>Portfolio</h1>
-                </NavLink>
+    updateToggleMobile();
+    window.addEventListener("resize", updateToggleMobile);
+    return () => window.removeEventListener("resize", updateToggleMobile);
+  }, []);
 
-                <NavLink className={({isActive}) => (isActive ? classes.active: '')} to='/about-me'>
-                    <h1>About Me</h1>
-                </NavLink>
+  const handleToggle = (toggle: boolean) => {
+    setToggleMobile(!toggle);
+  };
 
-                <NavLink className={({isActive}) => (isActive ? classes.active : '')} to='/testimonials'>
-                    <h1>Testimonials</h1>
-                </NavLink>
-            </nav>
+  return (
+    <div id="header" className={classes.header}>
+      <NavLink to="/">
+        <Logo imageUrl={logo} />
+      </NavLink>
+      <nav
+        className={`${classes.nav} ${
+          toggleMobile ? classes["nav-active"] : classes["nav-inactive"]
+        }`}
+      >
+        <NavLink
+          className={({ isActive }) => (isActive ? classes.active : "")}
+          to="/"
+        >
+          <h1>Home</h1>
+        </NavLink>
 
-            <a className={classes['download-cv']} target='-blank' rel='noopener noreferrer' href="/Elem Prosper Kachi CV.pdf">Download CV</a>
-            <div className={classes['mobile-toggle']}>
-                <div className={classes['icon-menu']}>
-                    <ImageWrapper sourceUrl={MenuOpen} alternativeText={'menu-toggle-icon'}/>
-                </div>
-                <div className={classes['icon-close-menu']}>
-                    <ImageWrapper sourceUrl={menuClose} alternativeText={'menu-toggle-icon'}/>
-                </div>
-            </div>
-        </div>
-    );
+        <NavLink
+          className={({ isActive }) => (isActive ? classes.active : "")}
+          to="/portfolios"
+        >
+          <h1>Portfolio</h1>
+        </NavLink>
 
-}
+        <NavLink
+          className={({ isActive }) => (isActive ? classes.active : "")}
+          to="/about-me"
+        >
+          <h1>About Me</h1>
+        </NavLink>
+
+        <NavLink
+          className={({ isActive }) => (isActive ? classes.active : "")}
+          to="/testimonials"
+        >
+          <h1>Testimonials</h1>
+        </NavLink>
+      </nav>
+
+      <a
+        className={classes["download-cv"]}
+        target="-blank"
+        rel="noopener noreferrer"
+        href="/Elem Prosper Kachi CV.pdf"
+      >
+        Download CV
+      </a>
+      <MobileToggle onToggle={handleToggle} />
+    </div>
+  );
+};
 
 export default Header;
---
+++ src/components/Layouts/MobileToggle.tsx
@@ -0,0 +1,54 @@
+import classes from "./MobileToggle.module.css";
+import ImageWrapper from "../UI/ImageWrapper";
+import menuClose from '../../assets/images/icon-close-menu.svg';
+import menuOpen from '../../assets/images/icon-menu.png';
+import { FC, useState, useEffect } from "react";
+
+interface MobileToggleProps {
+    onToggle: (toggleState: boolean) => void;
+  }
+
+const MobileToggle: FC<MobileToggleProps> = (props) => {
+  const [toggleState, setToggleState] = useState(false);
+
+    useEffect(() => {
+      const updateToggleState = () => {
+        const width = window.innerWidth;
+  
+        if (width <= 640) {
+          setToggleState(false);
+        } else {
+          setToggleState(true);
+        }
+      };
+  
+      updateToggleState();
+      window.addEventListener("resize", updateToggleState);
+      return () => window.removeEventListener("resize", updateToggleState);
+    }, []);
+
+  const handleToggleState = () => {
+    setToggleState(prevToggle => !prevToggle);
+
+    props.onToggle(toggleState);
+  }
+
+  return (
+    <div onClick={handleToggleState} className={classes["mobile-toggle"]}>
+      <div className={`${classes["icon-menu"]} ${toggleState ? classes.inactive : classes.active}`}>
+        <ImageWrapper
+          sourceUrl={menuOpen}
+          alternativeText="menu-toggle-icon"
+        />
+      </div>
+      <div className={`${classes["icon-close-menu"]} ${toggleState ? classes.active : classes.inactive}`}>
+        <ImageWrapper
+          sourceUrl={menuClose}
+          alternativeText="menu-toggle-icon"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MobileToggle;
